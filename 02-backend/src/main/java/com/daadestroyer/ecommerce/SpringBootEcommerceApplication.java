322dd43ca9005305747a847aa70ff9b3903817aa
daadestroyer <nigamshubham2000@gmail.com>
2021-05-15 17:00:59 +0530

    first commit

---
+++ 02-backend/src/main/java/com/daadestroyer/ecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.daadestroyer.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEcommerceApplication.class, args);
+    }
+
+}
