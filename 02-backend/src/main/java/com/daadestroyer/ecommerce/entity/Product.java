6259b7f2c41641a20edfb21a6ea5fa7e96c552b8
daadestroyer <nigamshubham2000@gmail.com>
2021-05-15 17:20:43 +0530

    product entity created...

---
+++ 02-backend/src/main/java/com/daadestroyer/ecommerce/entity/Product.java
@@ -0,0 +1,49 @@
+package com.daadestroyer.ecommerce.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+// This is lombok provide automatic getters and setters
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageURL;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "unit_in_stocks")
+    private int unitsInStocks;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+}
