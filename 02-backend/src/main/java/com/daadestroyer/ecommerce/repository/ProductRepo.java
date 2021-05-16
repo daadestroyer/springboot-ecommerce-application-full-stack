7bc3d410aa00a1e1433ff9cc57898fe76733f14c
daadestroyer <nigamshubham2000@gmail.com>
2021-05-17 01:01:15 +0530

    displaying data in angular...

--- 02-backend/src/main/java/com/daadestroyer/ecommerce/repository/ProductRepo.java
+++ 02-backend/src/main/java/com/daadestroyer/ecommerce/repository/ProductRepo.java
@@ -2,9 +2,9 @@ package com.daadestroyer.ecommerce.repository;
 
 import com.daadestroyer.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepo extends JpaRepository<Product, Long> {
 
 }
