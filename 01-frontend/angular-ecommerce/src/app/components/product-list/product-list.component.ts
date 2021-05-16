fb436de06a484647fdd4d4eede573d41db0db8bf
daadestroyer <nigamshubham2000@gmail.com>
2021-05-17 00:41:27 +0530

    displaying data in angular...

--- 01-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 01-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../entities/Product.spec';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.listProducts();
   }
 
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
--- 01-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 01-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Product } from '../entities/Product.spec';
+import { map } from 'rxjs/operators';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  // return an observable map the JSON data from Spring DATA REST to Product array 
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+// this interface unwraps the JSON from Spring DATA REST _embedded entry
+interface GetResponse {
+  _embedded: {
+      products: Product[];
+  }
 }
