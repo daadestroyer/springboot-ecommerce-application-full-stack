9f7ec2244b14a24a3f6e1cef0c4fe2e3ebfae8e8
daadestroyer <nigamshubham2000@gmail.com>
2021-07-04 23:55:07 +0530

    disable http path method and modify spring data rest detection strategy....

--- 02-backend/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -36,7 +36,8 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config , CorsRegistry cors) {
 
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST,
+                HttpMethod.DELETE , HttpMethod.PATCH};
 
         // disable HTTP methods for ProductCategory: PUT, POST and DELETE
         disableHttpMethods(Product.class, config, theUnsupportedActions);
--- 02-backend/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ 02-backend/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -4,10 +4,11 @@ import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
