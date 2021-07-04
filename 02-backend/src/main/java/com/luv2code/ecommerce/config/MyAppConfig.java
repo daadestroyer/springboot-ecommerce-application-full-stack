fe9bcbee816863e1228b2ba84f7d8a9c1a9d202b
daadestroyer <nigamshubham2000@gmail.com>
2021-07-04 23:37:40 +0530

    configure cors mapping for @RestController...

---
+++ 02-backend/src/main/java/com/luv2code/ecommerce/config/MyAppConfig.java
@@ -0,0 +1,24 @@
+package com.luv2code.ecommerce.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyAppConfig implements WebMvcConfigurer {
+
+    // allowed.origins=http://localhost:4200
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigins;
+
+    // spring.data.rest.base-path=/api
+    @Value("${spring.data.rest.base-path}")
+    private String[] basePath;
+
+    @Override
+    public void addCorsMappings(CorsRegistry cors) {
+        // setup cors mapping
+        cors.addMapping(basePath+"/**").allowedOrigins(theAllowedOrigins);
+    }
+}
--- 02-backend/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -5,6 +5,7 @@ import com.luv2code.ecommerce.entity.Product;
 import com.luv2code.ecommerce.entity.ProductCategory;
 import com.luv2code.ecommerce.entity.State;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -20,6 +21,9 @@ import java.util.Set;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Value("${allowed.origins}")
+    private String[] allowedOrigins;
 
     private EntityManager entityManager;
 
@@ -44,7 +48,8 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         exposeIds(config);
 
         // configure the cors mapping
-        cors.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+        // config.getBasePath() take the data of allowed.origins from application.properties
+        cors.addMapping(config.getBasePath()+"/**").allowedOrigins(allowedOrigins);
     }
 
     private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
